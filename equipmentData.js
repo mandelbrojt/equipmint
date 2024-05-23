c6049c17d1548a22b8ee5bf928409c818f5563fb
luis <luis.moreno@exatec.tec.mx>
2024-05-22 18:30:47 -0600

    Added sample data

---
+++ equipmentData.js
@@ -0,0 +1,92 @@
+export const equipmentArray = [
+    {
+        equipmentName: "Dell XPS Desktop 8960",
+        equipmentCategory: "desktops",
+        mainSpecs: ["spec1", "spec2", "spec3"],
+        id: 0,
+        price: 12345,
+        equipmentImage: '/path/to/somewhere',
+        productOfficialURL: 'wwww.somewhere.com'
+    },
+    {
+        equipmentName: "Equipment XYZ",
+        equipmentCategory: "ABC",
+        mainSpecs: ["spec1", "spec2", "spec3"],
+        id: 1,
+        price: 12345,
+        equipmentImage: '/path/to/somewhere',
+        productOfficialURL: 'wwww.somewhere.com'
+    },
+    {
+        equipmentName: "Equipment XYZ",
+        equipmentCategory: "ABC",
+        mainSpecs: ["spec1", "spec2", "spec3"],
+        id: 1,
+        price: 12345,
+        equipmentImage: '/path/to/somewhere',
+        productOfficialURL: 'wwww.somewhere.com'
+    },
+    {
+        equipmentName: "Equipment XYZ",
+        equipmentCategory: "ABC",
+        mainSpecs: ["spec1", "spec2", "spec3"],
+        id: 1,
+        price: 12345,
+        equipmentImage: '/path/to/somewhere',
+        productOfficialURL: 'wwww.somewhere.com'
+    },
+    {
+        equipmentName: "Equipment XYZ",
+        equipmentCategory: "ABC",
+        mainSpecs: ["spec1", "spec2", "spec3"],
+        id: 1,
+        price: 12345,
+        equipmentImage: '/path/to/somewhere',
+        productOfficialURL: 'wwww.somewhere.com'
+    },
+    {
+        equipmentName: "Equipment XYZ",
+        equipmentCategory: "ABC",
+        mainSpecs: ["spec1", "spec2", "spec3"],
+        id: 1,
+        price: 12345,
+        equipmentImage: '/path/to/somewhere',
+        productOfficialURL: 'wwww.somewhere.com'
+    },
+    {
+        equipmentName: "Equipment XYZ",
+        equipmentCategory: "ABC",
+        mainSpecs: ["spec1", "spec2", "spec3"],
+        id: 1,
+        price: 12345,
+        equipmentImage: '/path/to/somewhere',
+        productOfficialURL: 'wwww.somewhere.com'
+    },
+    {
+        equipmentName: "Equipment XYZ",
+        equipmentCategory: "ABC",
+        mainSpecs: ["spec1", "spec2", "spec3"],
+        id: 1,
+        price: 12345,
+        equipmentImage: '/path/to/somewhere',
+        productOfficialURL: 'wwww.somewhere.com'
+    },
+    {
+        equipmentName: "Equipment XYZ",
+        equipmentCategory: "ABC",
+        mainSpecs: ["spec1", "spec2", "spec3"],
+        id: 1,
+        price: 12345,
+        equipmentImage: '/path/to/somewhere',
+        productOfficialURL: 'wwww.somewhere.com'
+    },
+    {
+        equipmentName: "Equipment XYZ",
+        equipmentCategory: "ABC",
+        mainSpecs: ["spec1", "spec2", "spec3"],
+        id: 1,
+        price: 12345,
+        equipmentImage: '/path/to/somewhere',
+        productOfficialURL: 'wwww.somewhere.com'
+    },
+]
