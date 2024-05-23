82626ff122119d92c05c472aeb77421dda5bb691
luis <luis.moreno@exatec.tec.mx>
2024-05-22 19:03:54 -0600

    Added first product data

--- equipmentData.js
+++ equipmentData.js
@@ -1,92 +1,92 @@
 export const equipmentArray = [
     {
-        equipmentName: "Dell XPS Desktop 8960",
-        equipmentCategory: "desktops",
-        mainSpecs: ["spec1", "spec2", "spec3"],
+        name: "Dell XPS Desktop 8960",
+        category: "desktops",
+        specs: ["Intel Core i7", "16GB RAM", "1 TB SSD", "NVIDIA RTX 4060 Ti"],
         id: 0,
         price: 12345,
-        equipmentImage: '/path/to/somewhere',
-        productOfficialURL: 'wwww.somewhere.com'
+        thumbnail: '/img/products/dell-xps.png',
+        url: 'https://www.dell.com/en-us/shop/desktop-computers/new-xps-desktop/spd/xps-8960-desktop/usexpsthcto8960rpl21'
     },
     {
-        equipmentName: "Equipment XYZ",
-        equipmentCategory: "ABC",
-        mainSpecs: ["spec1", "spec2", "spec3"],
+        name: "Equipment XYZ",
+        category: "ABC",
+        specs: ["spec1", "spec2", "spec3"],
         id: 1,
         price: 12345,
-        equipmentImage: '/path/to/somewhere',
-        productOfficialURL: 'wwww.somewhere.com'
+        thumbnail: '/img/products',
+        url: 'wwww.somewhere.com'
     },
     {
-        equipmentName: "Equipment XYZ",
-        equipmentCategory: "ABC",
-        mainSpecs: ["spec1", "spec2", "spec3"],
-        id: 1,
+        name: "Equipment XYZ",
+        category: "ABC",
+        specs: ["spec1", "spec2", "spec3"],
+        id: 2,
         price: 12345,
-        equipmentImage: '/path/to/somewhere',
-        productOfficialURL: 'wwww.somewhere.com'
+        thumbnail: '/img/products',
+        url: 'wwww.somewhere.com'
     },
     {
-        equipmentName: "Equipment XYZ",
-        equipmentCategory: "ABC",
-        mainSpecs: ["spec1", "spec2", "spec3"],
-        id: 1,
+        name: "Equipment XYZ",
+        category: "ABC",
+        specs: ["spec1", "spec2", "spec3"],
+        id: 3,
         price: 12345,
-        equipmentImage: '/path/to/somewhere',
-        productOfficialURL: 'wwww.somewhere.com'
+        thumbnail: '/img/products',
+        url: 'wwww.somewhere.com'
     },
     {
-        equipmentName: "Equipment XYZ",
-        equipmentCategory: "ABC",
-        mainSpecs: ["spec1", "spec2", "spec3"],
-        id: 1,
+        name: "Equipment XYZ",
+        category: "ABC",
+        specs: ["spec1", "spec2", "spec3"],
+        id: 4,
         price: 12345,
-        equipmentImage: '/path/to/somewhere',
-        productOfficialURL: 'wwww.somewhere.com'
+        thumbnail: '/img/products',
+        url: 'wwww.somewhere.com'
     },
     {
-        equipmentName: "Equipment XYZ",
-        equipmentCategory: "ABC",
-        mainSpecs: ["spec1", "spec2", "spec3"],
-        id: 1,
+        name: "Equipment XYZ",
+        category: "ABC",
+        specs: ["spec1", "spec2", "spec3"],
+        id: 5,
         price: 12345,
-        equipmentImage: '/path/to/somewhere',
-        productOfficialURL: 'wwww.somewhere.com'
+        thumbnail: '/img/products',
+        url: 'wwww.somewhere.com'
     },
     {
-        equipmentName: "Equipment XYZ",
-        equipmentCategory: "ABC",
-        mainSpecs: ["spec1", "spec2", "spec3"],
-        id: 1,
+        name: "Equipment XYZ",
+        category: "ABC",
+        specs: ["spec1", "spec2", "spec3"],
+        id: 6,
         price: 12345,
-        equipmentImage: '/path/to/somewhere',
-        productOfficialURL: 'wwww.somewhere.com'
+        thumbnail: '/img/products',
+        url: 'wwww.somewhere.com'
     },
     {
-        equipmentName: "Equipment XYZ",
-        equipmentCategory: "ABC",
-        mainSpecs: ["spec1", "spec2", "spec3"],
-        id: 1,
+        name: "Equipment XYZ",
+        category: "ABC",
+        specs: ["spec1", "spec2", "spec3"],
+        id: 7,
         price: 12345,
-        equipmentImage: '/path/to/somewhere',
-        productOfficialURL: 'wwww.somewhere.com'
+        thumbnail: '/img/products',
+        url: 'wwww.somewhere.com'
     },
     {
-        equipmentName: "Equipment XYZ",
-        equipmentCategory: "ABC",
-        mainSpecs: ["spec1", "spec2", "spec3"],
-        id: 1,
+        name: "Equipment XYZ",
+        category: "ABC",
+        specs: ["spec1", "spec2", "spec3"],
+        id: 8,
         price: 12345,
-        equipmentImage: '/path/to/somewhere',
-        productOfficialURL: 'wwww.somewhere.com'
+        thumbnail: '/img/products',
+        url: 'wwww.somewhere.com'
     },
     {
-        equipmentName: "Equipment XYZ",
-        equipmentCategory: "ABC",
-        mainSpecs: ["spec1", "spec2", "spec3"],
-        id: 1,
+        name: "Equipment XYZ",
+        category: "ABC",
+        specs: ["spec1", "spec2", "spec3"],
+        id: 9,
         price: 12345,
-        equipmentImage: '/path/to/somewhere',
-        productOfficialURL: 'wwww.somewhere.com'
+        thumbnail: '/img/products',
+        url: 'wwww.somewhere.com'
     },
 ]
