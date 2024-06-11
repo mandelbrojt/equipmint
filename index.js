1df6e12b356b33ee07a1e2f1e5ecd3480dacf4ca
luis <luis.moreno@exatec.tec.mx>
2024-06-11 12:55:59 -0600

    Added event listeners for leasing term modal management

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-import { equipmentArray } from "./equipmentData"
+// import { equipmentArray } from "./equipmentData"
 
+/* DIALOG MANAGEMENT */
+const openLeasingDialogButton = document.getElementById("open-leasing-dialog")
+const leasingDialog = document.getElementById("leasing-dialog")
+const closeLeasingDialogButton = document.getElementById("close-leasing-dialog")
+
+openLeasingDialogButton.addEventListener("click", () => leasingDialog.showModal())
+closeLeasingDialogButton.addEventListener("click", () => leasingDialog.close())
