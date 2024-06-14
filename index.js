3b818ee93014ecce77cfcccb6368b768be0fabd1
luis <luis.moreno@exatec.tec.mx>
2024-06-13 23:25:45 -0600

    Renamed dialog product name element

--- index.js
+++ index.js
@@ -40,12 +40,12 @@ function populateLeasingDialog(equipmentId) {
     const filteredData = equipmentArray.filter(equipment => equipment.id === equipmentId)[0]
     // console.log(filteredData)
     const productSource = document.getElementById("leasing-dialog-product-source")
-    const productTitle = document.getElementById("leasing-dialog-product-title")
+    const productName = document.getElementById("leasing-dialog-product-title")
     const currentPrice = document.getElementById("leasing-dialog-current-price")
     const leasingTerm = document.getElementById("leasing-dialog-selected-term")
     
     productSource.setAttribute("href", filteredData.url)
-    productTitle.textContent = filteredData.name
+    productName.textContent = filteredData.name
     currentPrice.textContent = `${filteredData.price.toLocaleString("en-US")} USD`
     leasingTerm.textContent = `${defaultLeasingTerm} months`
 }
