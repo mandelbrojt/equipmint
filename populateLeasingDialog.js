a0db74e05132f187b32cbdb1e47cad311d2a8ebc
luis <luis.moreno@exatec.tec.mx>
2024-06-16 21:38:02 -0600

    Moved populateLeasingDialog function to its own file

---
+++ populateLeasingDialog.js
@@ -0,0 +1,17 @@
+import { equipmentArray } from "./equipmentData.js"
+
+const defaultLeasingTerm = 12
+
+export function populateLeasingDialog(equipmentId) {
+    const filteredData = equipmentArray.filter(equipment => equipment.id === equipmentId)[0]
+    // console.log(filteredData)
+    const productSource = document.getElementById("leasing-dialog-product-source")
+    const productName = document.getElementById("leasing-dialog-product-title")
+    const currentPrice = document.getElementById("leasing-dialog-current-price")
+    const leasingTerm = document.getElementById("leasing-dialog-selected-term")
+    
+    productSource.setAttribute("href", filteredData.url)
+    productName.textContent = filteredData.name
+    currentPrice.textContent = `${filteredData.price.toLocaleString("en-US")} USD`
+    leasingTerm.textContent = `${defaultLeasingTerm} months`
+}
