1c58e9cdb3e0d176a4b3cb29eae40b8032311ec2
luis <luis.moreno@exatec.tec.mx>
2024-06-19 10:35:13 -0600

    Refactored validateCoupon function and implemented warning/sucess messages during code validation

--- applyCouponCode.js
+++ applyCouponCode.js
@@ -1,14 +1,23 @@
 const validCoupon = "EQM20"
-let isCouponUsed = false
+const couponValidationMessage = document.getElementById("coupon-validation-message")
+const couponCodeInput = document.getElementById("coupon-code-input")
+const couponCodeValue = couponCodeInput.value
+let couponIsUsed = false
 
-export function validateCouponCode(couponCode) {
-    if (!isCouponUsed) {
-        if (couponCode === validCoupon) {
-            console.log("The coupon is valid...")
-        }
-        else {
-            console.log("The coupon is NOT valid...")
-        }
+export function applyCouponCode() {
+    const couponIsValid = validateCouponCode(couponCodeValue)
+
+    if (couponIsValid) {
+        couponValidationMessage.classList.toggle("successful-message")
+        couponValidationMessage.textContent = "Coupon is valid!"
+        couponIsUsed = true
+    }
+    else {
+        couponValidationMessage.classList.toggle("warning-message")
+        couponValidationMessage.textContent = "Invalid coupon. Please try again."
     }
 }
-function applyCouponCode() {}
+
+export function validateCouponCode(couponCode) {
+    return !couponIsUsed && couponCode === validCoupon;
+}
