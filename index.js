e60e90341e63569aae3d1f7a455ada82bd969ff5
luis <luis.moreno@exatec.tec.mx>
2024-06-26 17:59:53 -0600

    Removed close dialog button functionality

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 import { populateLeaseDialog } from "./leaseDialogManager.js"
 import { renderEquipments } from "./renderEquipments.js"
 
-// const leasingDialog = document.getElementById("leasing-dialog")
-// const closeLeasingDialogButton = document.getElementById("close-leasing-dialog")
-
 renderEquipments()
-
-// closeLeasingDialogButton.addEventListener("click", () => leasingDialog.close())
 
 document.addEventListener("click", function(event) {
     // console.log(event)
